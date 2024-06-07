c34372f96a3c75cd22d2345ef19f7cac8bb5c9ff
Shaun Donnelly <shaun.t.e.donnelly@gmail.com>
2024-06-07 16:46:40 +0100

    updated pyproject.toml

--- setup.py
+++ setup.py
@@ -6,19 +6,9 @@ setup(
     version="0.1.0",
     packages=find_packages(),
     install_requires=[
-        "requests>=2.32.3",
-        "colorlog>=2.32.3",
-        "tqdm>=4.66.4",
-        "numpy>=1.26.4",
-    ],
-    author="Dr. Shaun Thomas Edward Donnelly",
-    author_email="shaun.t.e.donnelly@gmail.com",
-    description="A utility for calling and using a basis set from ccrepo",
-    url="https://github.com/stedonnelly/ccrepo_utility",
-    license="MIT License (Non-Commercial)",
-    classifiers=[
-        "Programming Language :: Python :: 3.10",
-        "License :: OSI Approved :: MIT License",
-        "Operating System :: OS Independent",
+        "requests",
+        "colorlog",
+        "tqdm",
+        "numpy",
     ],
 )
