52975b735800b5c4a93a49ed4dfc1d7d2d979665
Shaun Donnelly <shaun.t.e.donnelly@gmail.com>
2024-07-23 10:44:01 +0100

    Updated init authors

--- ccrepo/__init__.py
+++ ccrepo/__init__.py
@@ -7,7 +7,7 @@ from .utils import convert_to_ccrepo, convert_to_basisopt_internal
 from .readers import read_basis_file
 from .writers import write_basis
 
-__author__ = "Dr. Shaun T. E. Donnelly, J. Dr. Grant Hill"
+__author__ = "Dr. Shaun T E Donnelly, J. Dr. Grant Hill"
 __author_email__ = "shaun.t.e.donnelly@gmail.com, grant.hill@sheffield.ac.uk"
 __license__ = "MIT"
 __url__ = "https://github.com/Sheffield-Theoretical-Chemistry/ccrepo_utility"
